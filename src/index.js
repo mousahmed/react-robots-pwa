6494d7fd163c71462a25dba527d0b2d8b81c065d
mousahmed <mous.ahmed@outlook.com>
2022-05-13 15:27:49 +0200

    3- optimizations for better performance and enabled service workter for the pwa

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import 'tachyons';
 
 
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorkerRegistration';
 import { requestRobots, searchRobots } from './reducers'
 
 import './index.css';
@@ -25,4 +25,5 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+
+serviceWorker.register()
